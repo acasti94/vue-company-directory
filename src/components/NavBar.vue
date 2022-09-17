0e566e753377d7384cf6ca8ab46c87d3200ee464
Adelson Castillo <adelsonramon@gmail.com>
2022-09-17 18:27:37 -0500

    added nav layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,16 +4,39 @@
 </script>
 
 <template>
-  <nav class="bg-red-500">
+  <nav>
     <div class="wrapper">
       <div class="brand">
         <span class="brand-title"> {{ brand }}</span>
       </div>
       <div class="menu">
-        <a href="#" class="menu-item">Departments </a>
+        <a href="#" class="menu-item">Departments</a>
         <a href="#" class="menu-item">Settings</a> 
-        <a href="#" class="menu-item">Logout</a>
+        <a href="#" class="menu-login">Logout</a>
       </div>
     </div>
   </nav>
 </template>
+
+<style scoped lang="postcss">
+    nav{
+        @apply flex h-20 bg-slate-900 text-slate-200;
+        .wrapper {
+            @apply container mx-auto flex w-full items-center justify-between;
+            .brand {
+                &-title{
+                @apply text-2xl font-bold text-yellow-500;
+                }
+            }
+        .menu {
+            @apply flex gap-2;
+            &-item{
+                @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+            }
+            &-login {
+                @apply rounded-md bg-red-500 px-4 py-2 hover:bg-red-700 hover:text-red-100;
+            }
+        }
+        }
+    }
+</style>
