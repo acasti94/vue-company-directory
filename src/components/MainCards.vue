5fee70b3c22aea9053a7a4531924f4a20ada07c8
Adelson Castillo <adelsonramon@gmail.com>
2022-09-17 21:50:05 -0500

    updated cards

---
+++ src/components/MainCards.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import MainCardSingle from './MainCardSingle.vue';
+</script>
+
+<template>
+    <div class="sub-wrapper">
+      <MainCardSingle v-for="n in 20" />
+    </div>
+</template>
+
+<style scoped lang="postcss">
+  .sub-wrapper {
+    @apply grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
+  }
+
+</style>
