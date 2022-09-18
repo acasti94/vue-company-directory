a9e992e335d9703cebf6708f72c6f69bace1f6e6
Adelson Castillo <adelsonramon@gmail.com>
2022-09-17 19:40:13 -0500

    search bar fixed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,14 @@
 </script>
 
 <template>
+    <div class="wrapper">
     <MainSearch />
-  <h1>Hello There</h1>
+    <h1>Hello There</h1>
+    </div>
 </template>
+
+<style scoped lang="postcss">
+    .wrapper {
+        @apply container mx-auto flex flex-col gap-4 py-4;
+    }
+</style>
--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Search Bar</h1>
+    <div class="wrapper">
+        <input type="text" placeholder="Search Staff Member" class="search" />
+    </div>
 </template>
+
+<style scoped lang="postcss">
+    .wrapper {
+        @apply relative flex w-full items-stretch;
+    .search {
+        @apply px-3 py-4 placeholder-slate-400 text-slate-700 rounded-md border-0 outline-none focus:ring focus:ring-orange-300;
+    }
+    }
+</style>
